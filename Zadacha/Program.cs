f10d845405269144794ac14be499d38c69c97fe3
Колесниченко Леонид <Leon-kol1994@rambler.ru>
2022-09-06 20:01:54 +0300

    Creating, filling and outputting an array

---
+++ Zadacha/Program.cs
@@ -0,0 +1,19 @@
+﻿void Zadacha()
+{
+    Console.WriteLine("Введите количество элементов массива");
+    int n = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[n];
+    Console.WriteLine("Заполните массив через 'пробел'");
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write(array[i] + "\t");
+    }
+
+
+
+}
+Zadacha();
