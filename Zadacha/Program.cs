f2018fc5be2f5b5459bbbd7b095a58e423a307e9
Колесниченко Леонид <Leon-kol1994@rambler.ru>
2022-09-06 20:55:26 +0300

    determining the size of the final array, its creation and filling

--- Zadacha/Program.cs
+++ Zadacha/Program.cs
@@ -2,18 +2,41 @@
 {
     Console.WriteLine("Введите количество элементов массива");
     int n = Convert.ToInt32(Console.ReadLine());
-    string[] array = new string[n];
-    Console.WriteLine("Заполните массив через 'пробел'");
+    string[] arrayStart = new string[n];
+    Console.WriteLine("Заполните массив через ENTER.");
     for (int i = 0; i < n; i++)
     {
-        array[i] = Console.ReadLine();
+        arrayStart[i] = Console.ReadLine();
     }
     for (int i = 0; i < n; i++)
     {
-        Console.Write(array[i] + "\t");
+        Console.Write(arrayStart[i] + "\t");
     }
+    Console.WriteLine();
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arrayStart[i].Length <= 3)
+        {
+            count += 1;
+        }
 
+    }
+    string[] arrayFinish = new string[count];
+    int ind = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arrayStart[i].Length <= 3)
+        {
+            arrayFinish[ind] = arrayStart[i];
+            ind++;
+        }
 
-
+    }
+    Console.WriteLine();
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(arrayFinish[i] + "\t");
+    }
 }
-Zadacha();
+Zadacha();determining the size of the final array, its creation and filling
